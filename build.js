c9b3092396e089399e56b4d1841f71d1879dc60d
David Gumm <gumm.david@icloud.com>
2024-04-25 21:27:50 -0700

    Update build.js and oracle-lite.js

--- build.js
+++ build.js
@@ -3,8 +3,12 @@
 require("esbuild")
     .build({
         logLevel: "info",
-        entryPoints: ["src/index.js"],
+        entryPoints: ["src/oracle-lite.ts"],
         bundle: true,
-        outfile: "www/main.js",
+        outfile: "dist/oracle-lite.js",
+        platform: 'browser', // Specify target platform
+        target: ['es2020', 'chrome58', 'firefox57', 'safari11'], // Specify ECMAScript target and browsers
+        sourcemap: true, // Optional: if you want source maps
+        minify: false, // Optional: if you want to minify the output
     })
     .catch(() => process.exit(1));
--- src/oracle-lite.js
+++ src/oracle-lite.js
@@ -1,25 +1,21 @@
 const determineOutcome = require("./oracle/lib/determine.outcome.js");
 
-// Every script needs a modifier function
-const modifier = (text) => {
-    const DEFAULT_CHANCE_FOR_SUCCESS = 0.5;
-    /**
-    * The main application logic to match commands.
-    */
-    const main = (chance) => {
-        const matchDefault = text.match(/(?:> (.*) (try|tries|attempt|attempts) )/i);
+const DEFAULT_CHANCE_FOR_SUCCESS = 0.5;
+/**
+* The main application logic to match commands.
+*/
+const main = (chance) => {
+    const matchDefault = text.match(/(?:> (.*) (try|tries|attempt|attempts) )/i);
 
-        if (matchDefault) {
-            const matchAtChance = text.match(/(?:> .* (?:try|tries|attempt|attempts) @(0?.\d+)) /i);
-            if (matchAtChance && matchAtChance[1]) {
-                chance = parseFloat(matchAtChance[1]);
-                text = text.replace("@" + matchAtChance[1] + " ", "");
-            }
-            return determineOutcome(chance, matchDefault[1], Math.random());
-        } else {
-            return "";
+    if (matchDefault) {
+        const matchAtChance = text.match(/(?:> .* (?:try|tries|attempt|attempts) @(0?.\d+)) /i);
+        if (matchAtChance[1]) {
+            chance = parseFloat(matchAtChance[1]);
+            text = text.replace("@" + matchAtChance[1] + " ", "");
         }
+        return determineOutcome(chance, matchDefault[1], Math.random());
+    } else {
+        return "";
     }
-    state.memory.frontMemory = main(DEFAULT_CHANCE_FOR_SUCCESS);
 }
-modifier(text);
+state.memory.frontMemory = main(DEFAULT_CHANCE_FOR_SUCCESS);
