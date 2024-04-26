7691fd594873b7e2c82e003e80d1cb1fae03306b
David Gumm <gumm.david@icloud.com>
2024-04-25 17:29:50 -0700

    Add esbuild dependency to package.json

---
+++ build.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+require("esbuild")
+    .build({
+        logLevel: "info",
+        entryPoints: ["src/index.js"],
+        bundle: true,
+        outfile: "www/main.js",
+    })
+    .catch(() => process.exit(1));
